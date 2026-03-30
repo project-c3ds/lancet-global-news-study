737a9d43e93735f957bc81973aa19def8989d356
iRanadheer <neneranadheer@gmail.com>
2026-03-30 15:46:10 +0100

    added batched token size.

--- start_vllm.sh
+++ start_vllm.sh
@@ -4,7 +4,7 @@
   --dtype auto \
   --max-model-len 8192 \
   --max-num-seqs 1024 \
-  --max-num-batched-tokens 65536 \
+  --max-num-batched-tokens 262144 \
   --gpu-memory-utilization 0.95 \
   --runner pooling \
   --pooler-config '{"pooling_type": "MEAN", "enable_chunked_processing": true, "max_embed_len": 32768}'
