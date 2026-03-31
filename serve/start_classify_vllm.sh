faa912e1495d5e00ae6d089531f88141e40b976d
iRanadheer <neneranadheer@gmail.com>
2026-03-31 18:15:34 +0100

    public

--- serve/start_classify_vllm.sh
+++ serve/start_classify_vllm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 .venv/bin/vllm serve iRanadheer/lancet_qwen35_4b_full_merged \
   --served-model-name lancet-classify \
+  --host 0.0.0.0 \
   --port 8000 \
   --dtype bfloat16 \
   --max-model-len 4096 \
