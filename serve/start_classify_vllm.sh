1f472642d2ef5e2a742e6cfeb2d62bde153f206d
iRanadheer <neneranadheer@gmail.com>
2026-03-31 19:37:22 +0100

    increase max-model-len to 8192 for classification server

--- serve/start_classify_vllm.sh
+++ serve/start_classify_vllm.sh
@@ -4,7 +4,7 @@
   --host 0.0.0.0 \
   --port 8000 \
   --dtype bfloat16 \
-  --max-model-len 4096 \
+  --max-model-len 8192 \
   --max-num-seqs 1024 \
   --max-num-batched-tokens 131072 \
   --gpu-memory-utilization 0.95
